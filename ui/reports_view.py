ee0b8d25b99d6b35770d51c452fdf51454066eaf
Pablo Bartolomé <pablobart.me@gmail.com>
2025-09-18 18:40:04 +0200

    Skeleton classes added for transaction_form and reports_view

--- ui/reports_view.py
+++ ui/reports_view.py
@@ -4,3 +4,14 @@
     Date: 16/09/2025
     Author: Pablo Bartolomé Molina
 '''
+from PyQt5.QtWidgets import QWidget
+
+class ReportsView(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def setup_ui(self):
+        pass
+
+    def plot_example(self):
+        pass
--- ui/transaction_form.py
+++ ui/transaction_form.py
@@ -4,3 +4,14 @@
     Date: 16/09/2025
     Author: Pablo Bartolomé Molina
 '''
+from PyQt5.QtWidgets import QDialog
+
+class TransactionForm(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def setup_ui(self):
+        pass
+
+    def save_transaction(self):
+        pass
